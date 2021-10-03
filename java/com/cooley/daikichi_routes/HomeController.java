e7b99d163df3c05171d087085921680e20e6fef5
Tabi <86167237+Cooleyt@users.noreply.github.com>
2021-10-03 11:58:48 -0400

    Add files via upload
    Updated routes

--- java/com/cooley/daikichi_routes/HomeController.java
+++ java/com/cooley/daikichi_routes/HomeController.java
@@ -1,5 +1,6 @@
 package com.cooley.daikichi_routes;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,20 @@ public class HomeController {
 	@RequestMapping("/tomorrow")
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!!";
+	}
+	
+	@RequestMapping("/m/{city}")
+	public String showCity(@PathVariable("city") String city) {
+		return "Congratulations! You will soon travel to:" + city;
+	}
+	
+	@RequestMapping("/lotto/{num}")
+	public String showLotto(@PathVariable("num") int num){
+		if(num%2 != 0) {
+			return "You will take a grand journey to the near future, but be wary of tempting offers.";
+			} 
+			else {
+				return "You have enjoyed the fruits of you labor but now is agreat time to spend time with your family and friends";
+		}
 	}
 }
